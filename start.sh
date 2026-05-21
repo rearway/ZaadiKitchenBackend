c73678fd57b23e176fbc1f6849e7df259c9360af
alameenkh <alameenkh7@gmail.com>
2026-05-21 09:12:04 +0530

    reverted seed

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ set -e
 echo "Starting database migrations..."
 npx sequelize-cli db:migrate
 
-echo "Running database seeders..."
-npx sequelize-cli db:seed:all
+# echo "Running database seeders..."
+# npx sequelize-cli db:seed:all
 
 echo "Migrations and seeds completed. Starting application..."
 node dist/main.js
+
