714796e2bef91a6f7c7b800e16b4abc2cf4c923f
alameen.kh <alameenkh7@gmail.com>
2026-05-22 12:35:19 +0000

    seed commented

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ set -e
 echo "Starting database migrations..."
 npx sequelize-cli db:migrate
 
-echo "Running database seeders..."
-npx sequelize-cli db:seed:all
+# echo "Running database seeders..."
+# npx sequelize-cli db:seed:all
 
 echo "Migrations and seeds completed. Starting application..."
 node dist/main.js
